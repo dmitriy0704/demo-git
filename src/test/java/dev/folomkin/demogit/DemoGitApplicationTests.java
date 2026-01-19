e743f4df6294d9ec0bb548ede6566b7445999894
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2026-01-19 19:44:51 +0300

    init commit

---
+++ src/test/java/dev/folomkin/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
